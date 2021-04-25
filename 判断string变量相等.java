8bf9e161fc98674ae772ad369a155386fcb6e41e
SiYuan-Lee <lisiyuan2017@gmail.com>
2021-04-25 15:02:52 +0800

    add database

---
+++ 判断string变量相等.java
@@ -0,0 +1,7 @@
+public class judge{
+    public static void main(String []args){
+        StringBuilder a=new StringBuilder("hello");
+        StringBuilder b=new StringBuilder("hello");
+        Systemp.out.println(a==b);
+    }
+}
